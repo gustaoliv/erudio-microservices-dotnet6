86060ba38eedcf51ab6fded2d86e201b465ec43e
Gustavo Oliveira <gusta.oliv4@gmail.com>
2023-01-20 14:26:12 -0300

    Small changes

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace RestWithASPNETUdemy.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class CalculatorController : ControllerBase
     {
         
